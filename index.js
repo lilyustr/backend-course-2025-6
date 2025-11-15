4fe610c4318031eec276ac0e2a4c733321323fad
Liliia Ustrytska bc2025-6 <liliyaustrytska@gmail.com>
2025-11-15 14:17:06 +0200

    Added commander, superagent, nodemon, .gitignore

---
+++ index.js
@@ -0,0 +1,11 @@
+import { Command } from "commander";
+const program = new Command();
+
+program
+  .requiredOption('-h, --host <host>', 'server host')
+  .requiredOption('-p, --port <port>', 'server port')
+  .requiredOption('-c, --cache <path>', 'cache directory');
+
+program.parse(process.argv);
+
+console.log("Arguments:", program.opts());
